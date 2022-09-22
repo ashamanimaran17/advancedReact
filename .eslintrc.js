180e60ba332fd502bff3da0d833004d5d19ff5db
ashamanimaran17 <ashamanimaran17@gmail.com>
2022-09-22 03:56:42 -0700

    ui and backend config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+};
