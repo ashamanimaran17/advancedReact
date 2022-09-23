dbf054bda8b5a56816771c63e500b1c1bb160f02
ashamanimaran17 <ashamanimaran17@gmail.com>
2022-09-23 10:42:58 -0700

    test cases running

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, use: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: ["/node_modules/"],
+      },
     ],
   },
   mode: "development",
